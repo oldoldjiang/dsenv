2c6be0b475dfe00cf04d310ee907105e1e125efa
oldoldjiang <oldoldjiang@qq.com>
2018-07-19 00:18:34 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
 sudo apt-get update
-sudo apt-get install r-base
-sudo apt-get install r-base-dev
+sudo apt-get -y install r-base
+sudo apt-get -y install r-base-dev
+sudo apt-get -y install libssl-dev libcurl4-openssl-dev
+sudo apt-get -y install libxml2-dev libzmq3-dev libpq-dev
+sudo apt-get -y install libhdf5-dev
