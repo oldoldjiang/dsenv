ee40310a4b755ac620f0b2c38bf134aa2f899919
oldoldjiang <oldoldjiang@qq.com>
2018-07-19 00:22:47 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,3 +4,7 @@ sudo apt-get -y install r-base-dev
 sudo apt-get -y install libssl-dev libcurl4-openssl-dev
 sudo apt-get -y install libxml2-dev libzmq3-dev libpq-dev
 sudo apt-get -y install libhdf5-dev
+
+sudo apt-get -y install mysql-server
+sudo apt -y install mysql-client
+sudo apt -y install libmysqlclient-dev
