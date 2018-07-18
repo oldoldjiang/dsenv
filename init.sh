1b90d47d276b6b7442504cec0a9e0a6fa334d257
oldoldjiang <oldoldjiang@qq.com>
2018-07-19 00:51:21 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,6 +5,11 @@ sudo apt-get -y install libssl-dev libcurl4-openssl-dev
 sudo apt-get -y install libxml2-dev libzmq3-dev libpq-dev
 sudo apt-get -y install libhdf5-dev
 
+cd ~
+cd Downlodas
+wget https://download1.rstudio.org/rstudio-xenial-1.1.453-amd64.deb
+gdebi rstudio-xenial-1.1.453-amd64.deb
+
 sudo apt-get -y install mysql-server
 sudo apt -y install mysql-client
 sudo apt -y install libmysqlclient-dev
