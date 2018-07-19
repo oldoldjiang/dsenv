5f720ee68b28770b6457a75db53006be0cb49089
oldoldjiang <oldoldjiang@qq.com>
2018-07-19 10:06:32 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,17 +5,31 @@ sudo apt-get -y install libssl-dev libcurl4-openssl-dev
 sudo apt-get -y install libxml2-dev libzmq3-dev libpq-dev
 sudo apt-get -y install libhdf5-dev
 
+
+
+# install Rstudio
+sudo apt -y install gdebi-core
 cd ~
 mkdir Downloads
 cd Downloads
 wget https://download1.rstudio.org/rstudio-xenial-1.1.453-amd64.deb
-gdebi rstudio-xenial-1.1.453-amd64.deb
+sudo gdebi rstudio-xenial-1.1.453-amd64.deb << EOF
+y
+EOF
 
 
 # install R packages
 R --no-save << EOF
 options(repos=structure(c(CRAN="https://cran.cnr.berkeley.edu/")))
 install.packages('gbm')
+install.packages('xts')
+install.packages('ggplot')
+install.packages('Rcpp')
+install.packages('data.table')
+install.packages('lubridate')
+install.packages('plyr')
+install.packages('reshape2')
+install.packages('profvis')
 EOF
 
 
